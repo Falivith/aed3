a52989d076ca0e5446c7568eef9c2349ec282229
Gabriel Ramires <falivith@gmail.com>
2023-03-28 15:32:27 -0300

    c++

---
+++ tsp_exact.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+    SetConsoleOutputCP(65001);
+
+    cout << 5;
+    cout << "Alô";
+    return 0;
+}
--- tsp_exato.c
+++ tsp_exato.c
@@ -2,8 +2,31 @@
 #include <stdlib.h>
 #include <windows.h>
 
+#define NUM_NODOS 11
+
 int main(){
+
     SetConsoleOutputCP(65001);
+
+    /*
+     * Abertura do arquivo para abrir um arquivo
+     * diferente, basta alterar o parâmetro do fopen();
+     */
+
+    FILE *fptr;
+    fptr = fopen("tsp1_253.txt", "r");
+
+    int matriz_adj[NUM_NODOS][NUM_NODOS];
+    int buffer;
+
+    for(int i = 0; i < NUM_NODOS; i++){
+        for(int j = 0; i<NUM_NODOS; j++){
+            fscanf(fptr, "%d", &buffer);
+            matriz_adj[i][j] = buffer; 
+        }
+    }
+
+
     printf("Hello World ã");
     return 0;
 }
